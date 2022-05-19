f53801525c41ae8234aed9bfab9900b18d13ba4e
enesa123 <enesagudzevic@gmail.com>
2022-05-19 19:14:38 +0200

    added new files

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var x=5;
+var y = x+3;
+var rec = 'neka rec';
+var rec2 = 'NEKA REC';
+var StringBroj = '23';
+var rezultat = StringBroj + 4;
---
+++ js1/index.js
@@ -0,0 +1,5 @@
+var godinaRodjenja= prompt("Unesite godinu rodjenja: ");
+var godina = prompt("Unesite godinu: ");
+godinaRodjenja= parseInt(godinaRodjenja);
+godina= parseInt(godina);
+console.log(godina - godinaRodjenja);
